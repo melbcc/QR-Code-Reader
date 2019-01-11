7a00f28c210038649808cd3eca3fd4f764a2cf2b
carryonrewardless <7085477+carryonrewardless@users.noreply.github.com>
2019-01-11 12:57:57 +1100

    Create ReadFromJSON.py

---
+++ ReadFromJSON.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import json
+
+with open('melbpc-members3.json', 'r') as file:
+  members =  json.load(file)
+  #print("Member count:", members['count'])
+  #print(members)
+  memberCount = members['count']
+  members = members['values']
+  #more = members['values']
+
+  for member in members.values():
+	for api_Membership_get in member.iteritems():
+		print("Contact id:", member['contact_id'], " Name:", member['first_name'], member['last_name'], "Post Code:", member['postal_code'], "membership No.:", member['custom_8'], "Expiry Date:", member['membership_type_id'])
+  #print(members)
