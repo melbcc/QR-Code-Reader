a45617f64453e42e28a8fe49717220b8636e3743
Peter Boin <Peter.Boin@gmail.com>
2019-01-17 22:51:24 +1100

    initial working REST API

---
+++ services/webapp/app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import inspect
+import re
+
+import argparse
+from flask import Flask, jsonify, abort
+
+# add ../lib folder
+_this_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+sys.path.insert(0, os.path.join(_this_path, '..', '..', 'lib'))
+
+from models import Member
+from databases import local
+
+# ----- Commandline Parameters
+parser = argparse.ArgumentParser(description='Web Application.')
+
+local.add_arguments(parser)
+
+args = parser.parse_args()
+
+
+
+# ----- Connect to Local Database
+session = local.get_session(
+	user=args.user,
+	password=args.password,
+	domain=args.domain,
+	port=args.port,
+	dbname=args.dbname,
+)
+
+
+# ----- Flask Application
+app = Flask(__name__)
+
+@app.route('/api/v1.0/member/<int:member_id>', methods=['GET'])
+def get_tasks(member_id):
+    # Query database
+    members = session.query(Member).filter_by(membshipnum=str(member_id))
+
+    # Validate result
+    if members.count() == 0:
+        abort(404)  # no entries found
+    elif members.count() > 1:
+        abort(500)  # more than one entry found (should not be possible)
+
+    # Return the member found
+    member = members.first()
+    return jsonify(member.as_dict)
+
+@app.route('/api/v1.0/coffeepot/request/<int:cup_count>', methods=['GET'])
+def get_coffee(cup_count):
+    if cup_count != 0:
+        abort(418)  # RFC 7168 compliance
+    return("Done!")
+
+
+app.run(debug=True)
