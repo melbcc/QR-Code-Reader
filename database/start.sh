856d25f6c3b2a3fdff5f14d2c5e87bb892b0a1a1
Peter Boin <Peter.Boin@gmail.com>
2019-01-26 21:39:38 +1100

    database persistence

--- database/start.sh
+++ database/start.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# create data folder
+if [ ! -d data ] ; then mkdir -p data ; fi
+
 docker run \
+    --volume ${PWD}/data:/var/lib/postgresql/data \
     -e POSTGRES_PASSWORD=secret \
     -e POSTGRES_DB=mydb \
     -p 5432:5432 \
