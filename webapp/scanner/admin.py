c972a70b61199289d3ee094be8293af674db1417
Peter Boin <Peter.Boin@gmail.com>
2019-01-26 23:52:46 +1100

    models searchable in admin

--- webapp/scanner/admin.py
+++ webapp/scanner/admin.py
@@ -10,18 +10,28 @@ class MemberAdmin(admin.ModelAdmin):
         'membership_num', 'contact_id',
         'first_name', 'last_name', 'status_id',
     )
+    search_fields = (
+        'membership_num', 'contact_id', 'first_name', 'last_name',
+    )
 
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('remote_key', 'name')
+    search_fields = ('name',)
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('remote_key', 'start_time', 'title', 'location')
+    search_fields = ('title', 'location__name')
 
 
 @admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
     list_display = ('event', 'member')
+    search_fields = (
+        'event__title',
+        'member__first_name', 'member__last_name',
+        'member__membership_num', 'member__contact_id',
+    )
