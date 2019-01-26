8aed2633cf1748e0c9a0eb9d7df70ca0365231a5
Peter Boin <Peter.Boin@gmail.com>
2019-01-26 22:39:12 +1100

    start from anywhere

--- database/start.sh
+++ database/start.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# ref: https://stackoverflow.com/questions/59895/#246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # create data folder
-if [ ! -d data ] ; then mkdir -p data ; fi
+if [ ! -d ${DIR}/data ] ; then mkdir -p ${DIR}/data ; fi
 
 docker run \
-    --volume ${PWD}/data:/var/lib/postgresql/data \
+    --volume ${DIR}/data:/var/lib/postgresql/data \
     -e POSTGRES_PASSWORD=secret \
     -e POSTGRES_DB=mydb \
     -p 5432:5432 \
