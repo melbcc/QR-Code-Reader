14093b914d73cc97162ec74a77a9e97bb011a6c5
Peter Boin <Peter.Boin@gmail.com>
2019-01-27 22:09:29 +1100

    config show actual locations and events

--- webapp/scanner/views.py
+++ webapp/scanner/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import generic
+from django.views import generic, View
+
+from .models import Location, Event
 
 
 class RootView(generic.TemplateView):
@@ -8,13 +10,33 @@ class RootView(generic.TemplateView):
 
 
 # ---------- Configure
-class ConfigLocationView(generic.TemplateView):
+class ConfigLocationView(View):
     template_name = 'config-location.html'
 
+    def get(self, request, *args, **kwargs):
+        # Locations QuerySet
+        locations = Location.objects.all()
 
-class ConfigEventsView(generic.TemplateView):
+        # Render & Return
+        return render(request, self.template_name, {
+            'locations': locations,
+        })
+
+
+class ConfigEventsView(View):
     template_name = 'config-events.html'
 
+    def get(self, request, *args, **kwargs):
+        # Get location and relevant events
+        location = Location.objects.filter(pk=int(request.GET['loc'])).first()
+        events = Event.objects.filter(location=location)
+        events = [e for e in events if e.is_upcoming]
+
+        # Render & Return
+        return render(request, self.template_name, {
+            'location': location,
+            'events': events,
+        })
 
 # ---------- Scanner
 class ScannerView(generic.TemplateView):
