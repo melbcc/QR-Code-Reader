6b3682c43a4cf25193c15ea911506494f5d2d7b3
Peter Boin <Peter.Boin@gmail.com>
2019-03-10 00:30:12 +1100

    changed sequence

--- webapp/scanner/static/scanner/js/scanner.js
+++ webapp/scanner/static/scanner/js/scanner.js
@@ -19,22 +19,43 @@ const jumpTo = (h) => {
     }
 }
 
-jumpTo.in = (h, timeout_ms) => {
+jumpTo.withTimeout = (h, timeout_ms) => {
     setTimeout(() => {
         jumpTo(h);
     }, timeout_ms);
 }
 
-/* ========== Local REST ========== */
-const getMember = async (contact_id) => {
-    // Clean up ID
-    // be tollerant of noise (like that from the QR code)
-    contact_id = contact_id.match(/\d*$/)[0];
-    // TODO: accept only a pure number, or a fully qualified URL.
-    //       otherwise return null
 
-    // Send Query
-    const response = await fetch('/api/members_cid/' + contact_id + '/');
+/* ========== Multimedia =========== */
+const playSound = (element_id) => {
+    $('#' + element_id)[0].play();
+}
+
+playSound.withTimeout = (element_id, timeout_ms) => {
+    setTimeout(() => {
+        playSound(element_id);
+    }, timeout_ms);
+}
+
+/* ========== Local REST ========== */
+/* ----- Member ----- */
+const getMember = async (contact_id, membership_num) => {
+    var response;
+    if (contact_id !== undefined) {
+        // Clean up ID
+        // be tollerant of noise (like that from the QR code)
+        contact_id = contact_id.toString();
+        contact_id = contact_id.match(/\d*$/)[0];
+        // TODO: accept only a pure number, or a fully qualified URL.
+        //       otherwise return null
+
+        // Send Query
+        response = await fetch('/api/members_cid/' + contact_id + '/');
+    } else {
+        // Send Query
+        membership_num = membership_num.toString();
+        response = await fetch('/api/members_memno/' + membership_num + '/');
+    }
 
     // Return Member object or null
     var member_obj = null;
@@ -46,10 +67,14 @@ const getMember = async (contact_id) => {
     return member_obj;
 }
 
+getMember.byContactID = (contact_id) => {return getMember(contact_id, undefined);}
+getMember.byMemberNum = (membership_num) => {return getMember(undefined, membership_num);}
 
+
+/* ----- Event ----- */
 const getEvent = async (pk) => {
     // Send Query
-    const response = await fetch('/api/events/' + pk + '/');
+    const response = await fetch('/api/events/' + pk.toString() + '/');
 
     // Return Event object or null
     var event_obj = null;
@@ -62,6 +87,7 @@ const getEvent = async (pk) => {
 }
 
 
+/* ----- Location ----- */
 const getLocationList = async () => {
     // Lists all locations (pull from REST)
     const response = await fetch('/api/locations/');
@@ -96,16 +122,16 @@ const resetPage = async () => {
     // Membership Number
     $('#memnumform input[type=text]').val('');
 
+    // Save error message
+    $('span.error_msg').text('');
     // Attendance hidden form (in "saving" state)
-    $('#attendanceform input[name=contact_id]').val('');
+    $('#attendanceform input[name=member_pk]').val('');
     $('#attendanceform input[name=event_pk]').val('');
 
     /* Move to top */
-    console.log(g_events);
-    if (g_events.length > 1) {
-        jumpTo("events"); // Select which event you're attending
-    } else {
-        jumpTo("scan"); // Only one event, just scan in
+    jumpTo("events");
+    if (g_events.length == 1) {
+        submitEvent();
     }
 }
 
@@ -121,12 +147,42 @@ resetPage.in = (timeout_ms) => {
 
 resetPage.cb_pending = false; // callback pending (used to cancel reset)
 
+/* ----- Event Selected ----- */
+const submitEvent = async () => {
+    // Get Selected (or only) Event
+    var event_obj;
+    if (g_events.length > 1) { // Multiple events, 1 must be selected
+        // Get selected event pk
+        var event_id = $('#eventform input[name=event]:checked').prop('id');
+        if (event_id === undefined) {
+            resetPage(); // no event was selected, reset for good measure
+            return;
+        }
+
+        // Get Event Object
+        var event_pk = event_id.match(/\d+$/)[0];
+        event_obj = await getEvent(event_pk);
+
+    } else {
+        // Only 1 event configured
+        event_obj = await getEvent(g_events[0]['pk']);
+    }
+
+    // Set page content baesd on Event
+    if (event_obj) { // confirm event exists
+        $('span.event_name').text(event_obj['title']);
+        $('#attendanceform input[name=event_pk]').val(event_obj['pk']);
+    }
+
+    jumpTo("scan");
+}
+
 /* ----- Scan contact_id ----- */
 const submitScan = async () => {
     /* Run when submitting the QR-Code */
     // Get member
     var qrtext = $('#scanform input[name=contact_id]').val();
-    const member_obj = await getMember(qrtext);
+    const member_obj = await getMember.byContactID(qrtext);
 
     if (member_obj) {
         // Inform user of status
@@ -134,65 +190,26 @@ const submitScan = async () => {
         $('span.last_name').text(member_obj['last_name']);
         $('span.status_name').text(member_obj['status']);
         // Set Member for attendance record
-        $('#attendanceform input[name=contact_id]').val(member_obj['contact_id']);
+        $('#attendanceform input[name=member_pk]').val(member_obj['pk']);
     } else {
         resetPage();
         // Do nothing; just reset the page
         return;
     }
 
-    if (g_events.length > 1) {
-        // Play sound
-        $('#sound_scan')[0].play();
-        if ($('#eventform input[name=event]:checked').length) {
-            // Event has already been selected
-            submitEvent();
-        } else {
-            jumpTo("events");
-        }
-    } else {
-        $('span.event_name').text(g_events[0]['title'])
-        $('#attendanceform input[name=event_pk]').val(g_events[0]['pk']);
-        // Play Sound
-        if (member_obj['status_isok']) {
-            $('#sound_scan')[0].play();
-        } else {
-            $('#sound_badmember')[0].play();
-        }
-        jumpTo("saving");
-        saveAttendance(member_obj['pk'], g_events[0]['pk']);
-    }
-}
-
-/* -----  ----- */
-const submitEvent = async () => {
-    // Get Selected Event
-    var event_id = $('#eventform input[name=event]:checked').prop('id');
-    if (event_id === undefined) {
-        // no event was selected, do nothing
-        return;
-    }
-
-    // Get Event Object
-    var event_pk = event_id.match(/\d+$/)[0];
-    var event_obj = await getEvent(event_pk);
-    if (event_obj) { // just checking it exists
-        $('span.event_name').text(event_obj['title']);
-        $('#attendanceform input[name=event_pk]').val(event_pk);
-    }
-
-    // Get Member Data
-    var member_obj = await getMember($('#attendanceform input[name=contact_id]').val());
-
+    // Play Sound
     if (member_obj['status_isok']) {
-        //$('#sound_scan')[0].play();
+        playSound('sound_scan');
     } else {
-        $('#sound_badmember')[0].play();
+        playSound('sound_error');
     }
 
+    // Record Attendance
     jumpTo("saving");
-    saveAttendance(member_obj['pk'], event_pk);
+    saveAttendance.fromForm();
 }
+
+/* ----- Membership Number ----- */
 
 const saveAttendance = (member_pk, event_pk) => {
     // Submit Attendance to REST API
@@ -203,15 +220,29 @@ const saveAttendance = (member_pk, event_pk) => {
     }
     console.log(attendance_obj);
 
-    $.post(
+    var response = $.post(
         "/api/attendance/",
         attendance_obj,
         (data) => {
             console.log(data);
         }
+    ).then(
+        (value) => { // success
+            jumpTo("welcome");
+            resetPage.in(3000); // 3 seconds
+        },
+        (reason) => { // failure
+            $('span.error_msg').text(reason.responseText);
+            console.error(reason);
+            playSound('sound_error');
+            resetPage.in(3000); // 3 seconds
+        }
     );
+}
 
-    // Get
-    jumpTo("welcome");
-    resetPage.in(3000); // 3 seconds
+saveAttendance.fromForm = () => {
+    saveAttendance(
+        $('#attendanceform input[name=member_pk]').val(),
+        $('#attendanceform input[name=event_pk]').val()
+    );
 }
