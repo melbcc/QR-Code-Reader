2c6d13bad5b3e3da3b8a11cdcfa00886aea432f0
Peter Boin <Peter.Boin@gmail.com>
2019-03-16 13:38:53 +1100

    initial hacky keyboard (WIP)

--- webapp/scanner/static/scanner/js/scanner.js
+++ webapp/scanner/static/scanner/js/scanner.js
@@ -1,4 +1,5 @@
 var err_lastActiveElement = undefined;
+var keyboard;
 
 /* ========== Setup =========== */
 const setupScannerPage = () => {
@@ -14,9 +15,44 @@ const setupScannerPage = () => {
         errorReset.in(50);
     });
 
+    // Keyboard
+    let Keyboard = window.SimpleKeyboard.default;
+
+    keyboard = new Keyboard({
+        onChange: input => onChange(input),
+        onKeyPress: button => onKeyPress(button)
+    });
+    document.querySelector(".kb-input").addEventListener("input", event => {
+        keyboard.setInput(event.target.value);
+    });
+    console.log(keyboard);
+
     // Start with Reset
     resetPage();
 };
+
+function onChange(input) {
+    document.querySelector(".kb-input").value = input;
+    console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") handleShift();
+}
+
+function handleShift() {
+    let currentLayout = keyboard.options.layoutName;
+    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+    keyboard.setOptions({
+      layoutName: shiftToggle
+    });
+}
 
 /* ========== Basic Navigation =========== */
 const defaultFormMap = {
