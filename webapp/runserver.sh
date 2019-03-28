74fc6aefdfd610016d6531e8599267af2efa0e5e
Peter Boin <Peter.Boin@gmail.com>
2019-03-28 16:27:57 +1100

    fix for "this directory" (WTF?)

--- webapp/runserver.sh
+++ webapp/runserver.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # ref: https://stackoverflow.com/questions/59895/#246128
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR=$(dirname $0)
 
 cd ${DIR}
 ./manage.py runserver
