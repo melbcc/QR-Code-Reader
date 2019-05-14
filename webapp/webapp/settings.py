e97978d2c086f726ec8a62d3f14609161b4a0a93
Peter Boin <Peter.Boin@gmail.com>
2019-05-14 22:20:57 +1000

    added static ip

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = 'sfirq7i#bdvynhn++8tvf(+bcm7%e007!li*v8%7km@3i++y07'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.81.61',  # static ip on MelbPC network (may change, only required to see remotely)
+]
 
 
 # Application definition
