2c22c40b6eea04d300bc53d16704770bcef9abe3
Peter Boin <Peter.Boin@gmail.com>
2019-05-14 22:16:14 +1000

    simplified

--- webapp/runserver-exposed.sh
+++ webapp/runserver-exposed.sh
@@ -2,7 +2,6 @@
 
 # ref: https://stackoverflow.com/questions/59895/#246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 
 cd ${DIR}
-./manage.py runserver $IP:8000
+./manage.py runserver 0.0.0.0:8000
