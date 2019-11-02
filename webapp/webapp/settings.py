0d605df024592e51aa6384ea6456012d9151c439
Peter Boin <Peter.Boin@gmail.com>
2019-11-02 15:08:25 +1100

    bump version (as a test)

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -14,7 +14,7 @@ import os
 from distutils.version import LooseVersion
 
 
-VERSION = LooseVersion('0.3.dev2')
+VERSION = LooseVersion('0.3.dev3')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
