b6c1f80c63f50d2a5df32682933f2ec758f48b27
Peter Boin <peter.boin@gmail.com>
2021-08-29 14:17:51 +1000

    fun with scanner overlay

--- frontend/src/views/Scan.vue
+++ frontend/src/views/Scan.vue
@@ -7,12 +7,26 @@
       </div>
     </qrcode-stream>
     result: {{ result }}<br/>
+    type: {{ resultType }} <br/>
     {{ error }}
   </div>
 </template>
 
 <script>
   import { QrcodeStream } from 'qrcode-reader-vue3'
+
+  function getMemberNumber(decodedText) {
+    if (typeof decodedText === 'string') {
+      const match = decodedText.match(/(?<type>M)?(?<number>\d+)$/i);
+      if (match) {
+        return {
+          type: match.groups.type ? 'member' : 'contact',
+          number: parseInt(match.groups.number),
+        }
+      }
+    }
+  }
+
   export default {
     components: {
       QrcodeStream,
@@ -21,6 +35,7 @@
       return {
         loading: true,
         result: "",
+        resultType: null,
         error: "",
       }
     },
@@ -28,25 +43,32 @@
       navPrev() { this.$router.push('/select') },
       navNext() { this.$router.push('/list') },
       async onDecode(decoded) {
-        this.result = decoded
+        this.result = decoded;
       },
       async onInit (promise) {
         // Error Lokup:
         // https://gruhn.github.io/vue-qrcode-reader/api/QrcodeStream.html#events
         try {
-          await promise
+          await promise;
         } catch (error) {
-          this.error = error.name
+          this.error = error.name;
         } finally {
-          this.loading = false
+          this.loading = false;
         }
       },
       paintOutline (detectedCodes, ctx) {
         // copied from: https://gruhn.github.io/vue-qrcode-reader/demos/CustomTracking.html
         for (const detectedCode of detectedCodes) {
-          const [ firstPoint, ...otherPoints ] = detectedCode.cornerPoints
+          const [ firstPoint, ...otherPoints ] = detectedCode.cornerPoints;
+          const { boundingBox, rawValue } = detectedCode;
+          const memberNum = getMemberNumber(rawValue);
 
-          ctx.strokeStyle = "red";
+          // Component Data
+          this.resultType = (memberNum ? memberNum.type : "invalid").toUpperCase();
+
+          // Boundary Line
+          ctx.strokeStyle = memberNum ? "dodgerblue" : "red";
+          ctx.lineWidth = boundingBox.width / 20;
 
           ctx.beginPath();
           ctx.moveTo(firstPoint.x, firstPoint.y);
@@ -56,6 +78,20 @@
           ctx.lineTo(firstPoint.x, firstPoint.y);
           ctx.closePath();
           ctx.stroke();
+
+          // Floating Text
+          const centerX = boundingBox.x + boundingBox.width / 2;
+          const centerY = boundingBox.y + boundingBox.height / 2;
+          const fontSize = Math.max(12, 50 * boundingBox.width / ctx.canvas.width);
+
+          ctx.font = `bold ${fontSize}px sans-serif`;
+          ctx.textAlign = 'center';
+
+          ctx.lineWidth = boundingBox.width / 30;
+          ctx.strokeText(this.resultType, centerX, centerY);
+
+          ctx.fillStyle = 'white';
+          ctx.fillText(this.resultType, centerX, centerY)
         }
       },
     },
