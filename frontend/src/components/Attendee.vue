85368328f69b505680e9e2a2f515f31be3b44fe8
Peter Boin <peter.boin@gmail.com>
2021-09-19 00:19:52 +1000

    member status in attendee list

--- frontend/src/components/Attendee.vue
+++ frontend/src/components/Attendee.vue
@@ -5,7 +5,16 @@
             <div v-if="obj.export_time"><i class="fas fa-check-circle"/></div>
             <div v-else><i class="far fa-check-circle"/></div>
         </span>
-
+        <span class="membership-status">
+            <div v-if="obj.contact.membership?.status=='Deceased'" class="icon-graphic status-deceased"><i class="fas fa-user-alt-slash"/></div>
+            <div v-else-if="obj.contact.membership?.status=='Cancelled'" class="icon-graphic status-cancelled"><i class="fas fa-ban"/></div>
+            <div v-else-if="obj.contact.membership?.status=='Pending'" class="icon-graphic status-pending"><i class="fas fa-hourglass-half"/></div>
+            <div v-else-if="obj.contact.membership?.status=='Expired'" class="icon-graphic status-expired"><i class="fas fa-ban"/></div>
+            <div v-else-if="obj.contact.membership?.status=='Grace'" class="icon-graphic status-grace"><i class="fas fa-hourglass-half"/></div>
+            <div v-else-if="obj.contact.membership?.status=='Current'" class="icon-graphic status-current"><i class="fas fa-check-circle"/></div>
+            <div v-else-if="obj.contact.membership?.status=='New'" class="icon-graphic status-new"><i class="fas fa-plus-circle"/></div>
+            <div v-else class="icon-graphic status-unknown"><i class="fas fa-question"/></div>
+        </span>
     </div>
 </template>
 
@@ -24,9 +33,23 @@
 <style lang="scss" scoped>
     .attendee {
         font-size: 1.5em;
+        .membership-status {
+            float: right;
+            margin-right: 0.5em;
+            // TODO: import membership status colours from common definition
+            .status-deceased  { color: grey;       }
+            .status-cancelled { color: red;        }
+            .status-pending   { color: grey;       }
+            .status-expired   { color: red;        }
+            .status-grace     { color: grey;       }
+            .status-current   { color: limegreen;  }
+            .status-new       { color: grey;       }
+            .status-unknown   { color: grey;       }  // set if status is anything else
+        }
         .sync-status {
             float: right;
             margin-right: 0.5em;
+            color: dodgerblue;
         }
     }
 </style>
