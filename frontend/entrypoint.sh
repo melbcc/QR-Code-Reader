1d4f76d4d0c50cfed8f10ec9d50eba0086c9f87e
Peter Boin <peter.boin@gmail.com>
2021-11-07 17:04:12 +1100

    force build output dir to be writable

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+if [ -d "$VUE_OUTPUT_DIR" ] ; then
+    # Set output directory to all access
+    # Why?: because vue build runs with host's user id (`id -u`)
+    #   who does not have write access.
+    chmod -R 777 "$VUE_OUTPUT_DIR"
+    # FIXME: I'm sure there's a better way to do this.
+fi
 npm install
 exec "$@"
