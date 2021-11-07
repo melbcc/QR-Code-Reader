b19c385a6b30134fe62f767c4ed229d7a5972871
Peter Boin <peter.boin@gmail.com>
2021-11-07 19:51:09 +1100

    don't make files executable

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,7 +3,8 @@ if [ -d "$VUE_OUTPUT_DIR" ] ; then
     # Set output directory to all access
     # Why?: because vue build runs with host's user id (`id -u`)
     #   who does not have write access.
-    chmod -R 777 "$VUE_OUTPUT_DIR"
+    find $VUE_OUTPUT_DIR -type d exec chmod ugo+rwx {} +
+    find $VUE_OUTPUT_DIR -type f exec chmod ugo+rw {} +
     # FIXME: I'm sure there's a better way to do this.
 fi
 npm install
