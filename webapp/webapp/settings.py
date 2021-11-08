f3eaef62b5b8f6998c502d94318459b497e88f56
Peter Boin <peter.boin@gmail.com>
2021-11-08 22:13:27 +1100

    updated dev environment

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '0').lower() in ('1', 'yes', 'on', 'enabled')
 
-ALLOWED_HOSTS = [
-    os.environ.get('DJANGO_ALLOWED_HOST', 'localhost'),
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = os.environ.get(
+    'DJANGO_ALLOWED_HOSTS',
+    '127.0.0.1,localhost'
+).split(',')
 
 # Application definition
 
