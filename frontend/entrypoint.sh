6c54ade9647d3009cf5e396d979089f8b35c0260
Peter Boin <peter.boin@gmail.com>
2021-11-08 19:53:14 +1100

    bugfix: need to control perms of output dir's parent

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if [ -d "$VUE_OUTPUT_DIR" ] ; then
+if [ -d "$(dirname $VUE_OUTPUT_DIR)" ] ; then
     # Set output directory to all access
     # Why?: because vue build runs with host's user id (`id -u`)
     #   who does not have write access.
-    find $VUE_OUTPUT_DIR -type d exec chmod ugo+rwx {} +
-    find $VUE_OUTPUT_DIR -type f exec chmod ugo+rw {} +
+    find $(dirname $VUE_OUTPUT_DIR) -type d -exec chmod ugo+rwx {} +
+    find $(dirname $VUE_OUTPUT_DIR) -type f -exec chmod ugo+rw {} +
     # FIXME: I'm sure there's a better way to do this.
 fi
 npm install
