04b0dee386894c53ae37b5f4009c9f61748fa4eb
Peter Boin <peter.boin@gmail.com>
2021-11-27 16:46:09 +1100

    dev certs expire in 1 year (not that it matters)

--- env/staged-letsencrypt/live/localhost/generate-certs.sh
+++ env/staged-letsencrypt/live/localhost/generate-certs.sh
@@ -3,7 +3,7 @@ set -e
 rsa_key_size=4096
 path=$(dirname $0)
 
-openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 \
+openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 365 \
     -keyout "$path/privkey.pem" \
     -out "$path/fullchain.pem" \
     -subj "/CN=localhost"
