c6a80c77750d05492fa40a05cdd5b54291bda5b2
Peter Boin <peter.boin@gmail.com>
2021-11-27 17:06:01 +1100

    dev proxy not required

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -11,20 +11,6 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     port: 8080,
-    proxy: {
-      "/api": {
-        target: `http://${process.env.API_HOST || "127.0.0.1:8080"}`,
-        changeOrigin: true,
-        secure: false,
-        logLevel: 'debug',
-      },
-      "/token": {
-        target: `http://${process.env.API_HOST || "127.0.0.1:8080"}`,
-        changeOrigin: true,
-        secure: false,
-        logLevel: 'debug',
-      },
-    },
     progress: false, // ref: https://github.com/vuejs/vue-cli/issues/4557#issuecomment-545965828
   },
   outputDir: process.env.VUE_OUTPUT_DIR || "dist",
